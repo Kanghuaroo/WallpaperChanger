3b5e9ada42f81f9bf1964770db8910bcebaa2144
David Miller <5035dcmiller@gmail.com>
2020-02-08 17:39:41 -0500

    added time check

--- WallpaperChanger.py
+++ WallpaperChanger.py
@@ -1,8 +1,15 @@
 import os
 import random 
+import time
 
-
-pics = os.listdir('/home/lyrk/Pictures/Wallpapers/')
-path = "/home/lyrk/Pictures/Wallpapers/" + random.choice(pics)
+currHour = time.time()[3]
+if currHour < 10:
+	picFolder = os.listdir('/home/lyrk/Pictures/Wallpapers/')
+elif currHour > 8:
+	picFolder = os.listdir('/home/lyrk/Pictures/Wallpapers/')
+else:
+	picFolder = os.listdir('/home/lyrk/Pictures/Wallpapers/')
+	
+path = "/home/lyrk/Pictures/Wallpapers/" + random.choice(picFolder)
 
 os.system("gsettings set org.gnome.desktop.background picture-uri file:" + path)
