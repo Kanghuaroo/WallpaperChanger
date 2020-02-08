5d8c69487a38236b430e1a14be5428044631dab1
David Miller <5035dcmiller@gmail.com>
2020-02-08 18:29:44 -0500

    Added folder choosing based off of time

--- WallpaperChanger.py
+++ WallpaperChanger.py
@@ -2,14 +2,15 @@ import os
 import random 
 import time
 
-currHour = time.time()[3]
+currHour = time.localtime()[3]
 if currHour < 10:
-	picFolder = os.listdir('/home/lyrk/Pictures/Wallpapers/')
+	path = '/home/lyrk/Pictures/Wallpapers/Morning/'
 elif currHour > 8:
-	picFolder = os.listdir('/home/lyrk/Pictures/Wallpapers/')
+	path = '/home/lyrk/Pictures/Wallpapers/Night/'
 else:
-	picFolder = os.listdir('/home/lyrk/Pictures/Wallpapers/')
-	
-path = "/home/lyrk/Pictures/Wallpapers/" + random.choice(picFolder)
+	path = '/home/lyrk/Pictures/Wallpapers/'
+
+picFolder = os.listdir(path)	
+path +=  random.choice(picFolder)
 
 os.system("gsettings set org.gnome.desktop.background picture-uri file:" + path)
