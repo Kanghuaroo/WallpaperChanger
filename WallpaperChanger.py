26869c2776b72d84c722194b0a73c1257dd29ce0
David Miller <5035dcmiller@gmail.com>
2022-04-19 12:44:10 -0400

    added arg parse and param for time

--- WallpaperChanger.py
+++ WallpaperChanger.py
@@ -1,21 +1,48 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import os
 import random 
 import time
 import getpass
+import argparse
 
-user = getpass.getuser()
-currHour = time.localtime()[3]
 
-if currHour <= 10:
-	path = '/home/' + user + '/Pictures/Wallpapers/Morning/'
-elif currHour >= 20:
-	path = '/home/' + user + '/Pictures/Wallpapers/Night/'
-else:
-	path = '/home/' + user + '/Pictures/Wallpapers/Day/'
+def getDir(override):
+    user = getpass.getuser()
+    if override == -1:
+        currHour = time.localtime()[3]
+    else:
+        currHour = override
 
-picFolder = os.listdir(path)	
-path += random.choice(picFolder)
+    if currHour < 10:
+            path = '/home/' + user + '/Pictures/Wallpapers/Morning/'
+    elif currHour >= 20:
+            path = '/home/' + user + '/Pictures/Wallpapers/Night/'
+    else:
+            path = '/home/' + user + '/Pictures/Wallpapers/Day/'
+    return path
 
-os.system("gsettings set org.gnome.desktop.background picture-uri file:" + path)
+def main(args):
+    path = getDir(args.time)
+
+    picFolder = os.listdir(path)	
+    path += random.choice(picFolder)
+
+    os.system("gsettings set org.gnome.desktop.background picture-uri file:" + path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+            '-t', '--time',
+            help='what time you want to override with',
+            type=int,
+            default = -1)
+    #TODO not implemented
+    parser.add_argument(
+            '-r', '--random',
+            help="Can randomly pick from ALL availible background photos",
+            type=bool,
+            default=False)
+
+    args = parser.parse_args()
+    main(args)
